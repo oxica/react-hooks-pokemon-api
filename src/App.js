a3a3b173d780737c1879f5a69e043fdfc697fa4a
Oksana <kskaplun@gmail.com>
2022-05-28 16:04:34 +0300

    refactoring to hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import PokemonForm from './components/PokemonForm';
 import PokemonInfo from './components/PokemonInfo';
 
-export default class App extends Component {
-  state = {
-    pokemonName: '',
-  };
+export default function App() {
+  const [pokemonName, setPokemonName] = useState('');
 
-  handleFormSubmit = pokemonName => {
-    this.setState({ pokemonName });
-  };
-
-  render() {
-    return (
-      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-        <PokemonInfo pokemonName={this.state.pokemonName} />
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
-  }
+  return (
+    <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+      <PokemonForm onSubmit={setPokemonName} />
+      <PokemonInfo pokemonName={pokemonName} />
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
 }
